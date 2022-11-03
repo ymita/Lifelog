dda281b174c06ca4861e682924e877c0ebc4da82
Yuki Mita <ymita@infragistics.com>
2022-11-03 18:09:59 +0900

    Add Card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
 import Link from "next/link";
 import { usePosts } from "../hooks/usePosts";
 import { Post } from "../models/post";
@@ -8,13 +15,27 @@ export default function Home() {
 
   return (
     <>
-      <ul>
-        {posts.map((post: Post) => (
-          <li key={post.id}>
-            {post.title} / {post.author} / {post.description}
-          </li>
-        ))}
-      </ul>
+      {posts.map((post: Post) => (
+        <Card sx={{ minWidth: 275, border: "1px solid lightgray", margin: "1rem" }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {post.title}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              author: {post.author}
+            </Typography>
+            <Typography variant="body2">{post.description}</Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Read More</Button>
+          </CardActions>
+        </Card>
+
+        // <li key={post.id}>
+        //     {post.title} / {post.author} / {post.description}
+        //   </li>
+      ))}
+
       <div>
         <Link href="/about">About</Link>
       </div>
