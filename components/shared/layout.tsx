457164423666f51369049164888353b293ff951e
Yuki Mita <ymita@infragistics.com>
2022-11-03 17:40:50 +0900

    Add MUI AppBar

--- components/shared/layout.tsx
+++ components/shared/layout.tsx
@@ -1,11 +1,32 @@
+import { Box, AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 export default function Layout({ children }: Props) {
   return (
     <div>
-      <div>header</div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Lifelog
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <main>
         <main>{children}</main>
       </main>
